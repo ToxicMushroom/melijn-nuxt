c3cc796ee39f7db2fa22d6d0ff531d3f106dbab4
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-02-28 14:47:49 +0100

    fix: make footer date auto-update

--- components/footer.vue
+++ components/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="foot">
     <div class="left">
-      © 2020, ToxicMushroom#2610. All rights reserved.
+      © {{ new Date().getFullYear() }}, ToxicMushroom#2610. All rights reserved.
     </div>
 
     <div class="right">
